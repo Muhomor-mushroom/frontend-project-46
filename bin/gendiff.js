91ff6a291da57caeabc04c2492f9268096c13f57
AX <ax@MacBook-Pro-NS.local>
2024-10-13 15:39:11 +0300

    second commit

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,11 @@ program
     .description('Compares two configuration files and shows a difference.')
 
 program
+    .argument('<filepath1>')
+    .argument('<filepath1>')
+program
     .option('-v, --version', 'output the version number')
+    .option('-f, --format [type]', 'output format')
     .helpOption('-h, --help', 'output usage information')
     
     program.parse();
