55242c2312238b6f7af7497985822375b228ea9c
AX <ax@MacBook-Pro-NS.local>
2024-10-15 23:18:00 +0300

    four step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -11,10 +11,10 @@ program
   .option('-f, --format [type]', 'output format')
   .helpOption('-h, --help', 'output usage information')
   .action((filepath1, filepath2) => {
-    const myCWD = cwd()
+    const myCWD = cwd();
     console.log(`your path now: ${myCWD}`);
     console.log(`first path is ${filepath1}, second path is ${filepath2}`)
-    showDiff(path.resolve(`${myCWD}/${filepath1}`), path.resolve(`${myCWD}${filepath2}`))
+    showDiff(path.resolve(`${myCWD}`, `${filepath1}`), path.resolve(`${myCWD}`, `${filepath2}`))
   });
 
 program.parse();
