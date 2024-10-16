e2ce717b4a752dad32fb9c9008e8da27b1365044
AX <ax@MacBook-Pro-NS.local>
2024-10-16 21:39:04 +0300

    making test

--- __tests__/showDiff.test.js
+++ __tests__/showDiff.test.js
@@ -1,6 +1,9 @@
 import showDiff from '../src/showDiff.js';
 import showDiffExpected from '../__fixtures__/showDiffExpected.js';
+import path from 'node:path';
+import { cwd } from 'node:process';
 
 test('showDiff', () => {
-  expect(showDiff(`/Users/ax/Desktop/2 project/frontend-project-46/file1.json`, `/Users/ax/Desktop/2 project/frontend-project-46/file2.json`)).toEqual(showDiffExpected);
+  const myCWD = cwd();
+  expect(showDiff(path.resolve(`${myCWD}`, `file1.json`), path.resolve(`${myCWD}`, `file2.json`))).toEqual(showDiffExpected);
 });
