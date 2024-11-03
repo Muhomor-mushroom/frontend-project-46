1a2ca74234958cb51830e98756935c3e6525fd61
AX <ax@MacBook-Pro-NS.local>
2024-11-03 18:24:23 +0300

    seven step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,10 +10,11 @@ program
   .option('-v, --version', 'output the version number')
   .option('-f, --format [type]', 'output format')
   .helpOption('-h, --help', 'output usage information')
-  .action((options, filepath1, filepath2) => {
+  .action((option, filepath1, filepath2) => {
     const myCWD = cwd();
+    const opts = program.opts()
     console.log(`first path is ${filepath1}, second path is ${filepath2}`);
-    showDiff(path.resolve(`${myCWD}`, `${filepath1}`), path.resolve(`${myCWD}`, `${filepath2}`), `${options.format}`);
+    showDiff(path.resolve(`${myCWD}`, `${filepath1}`), path.resolve(`${myCWD}`, `${filepath2}`), `${opts.format}`);
   });
 
 program.parse();
