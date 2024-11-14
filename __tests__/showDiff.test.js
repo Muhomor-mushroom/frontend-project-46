9da7decad53cae3d5e7ce5bcfbc6f455c824bcad
AX <ax@MacBook-Pro-NS.local>
2024-11-14 14:35:34 +0300

    pre-final

--- __tests__/showDiff.test.js
+++ __tests__/showDiff.test.js
@@ -5,33 +5,29 @@ import showDiffExpected from '../__fixtures__/showDiffExpected.js';
 import showDiffPlainExpected from '../__fixtures__/showDiffPlainExpected.js';
 import showDiffJsonExpected from '../__fixtures__/showDiffJsonExpected.js';
 
+const myCWD = cwd();
+
 test('showDiffJsons', () => {
-  const myCWD = cwd();
   const result = showDiff(path.resolve(myCWD, '__fixtures__/file1.json'), path.resolve(myCWD, '__fixtures__/file2.json'));
   expect(result).toEqual(showDiffExpected());
 });
 test('ShowDiffYamls', () => {
-  const myCWD = cwd();
   const result = showDiff(path.resolve(myCWD, '__fixtures__/file1.yml'), path.resolve(myCWD, '__fixtures__/file2.yml'));
   expect(result).toEqual(showDiffExpected());
 });
 test('ShowDiffPlainJson', () => {
-  const myCWD = cwd();
   const result = showDiff(path.resolve(myCWD, '__fixtures__/file1.json'), path.resolve(myCWD, '__fixtures__/file2.json'), 'plain');
   expect(result).toEqual(showDiffPlainExpected());
 });
 test('ShowDiffPlainYamls', () => {
-  const myCWD = cwd();
   const result = showDiff(path.resolve(myCWD, '__fixtures__/file1.yml'), path.resolve(myCWD, '__fixtures__/file2.yml'), 'plain');
   expect(result).toEqual(showDiffPlainExpected());
 });
 test('ShowDiffJsonInJson', () => {
-  const myCWD = cwd();
   const result = showDiff(path.resolve(myCWD, '__fixtures__/file1.json'), path.resolve(myCWD, '__fixtures__/file2.json'), 'json');
   expect(result).toEqual(showDiffJsonExpected());
 });
 test('ShowDiffPlainYamlsInJson', () => {
-  const myCWD = cwd();
   const result = showDiff(path.resolve(myCWD, '__fixtures__/file1.yml'), path.resolve(myCWD, '__fixtures__/file2.yml'), 'json');
   expect(result).toEqual(showDiffJsonExpected());
 });
